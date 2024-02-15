6bd07667712b5546b86f116f013c29d66a5495eb
iveschatelin <iveschatelin@gmail.com>
2024-02-15 17:28:24 +0100

    initial commit

---
+++ projet.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+from flask_cors import CORS
+from flask_swagger_ui import get_swaggerui_blueprint
+from datetime import datetime
+import sqlite3
+from markupsafe import escape
+import sys
+import pandas as pd
+from flask import jsonify
+import redis
+
+app = Flask(__name__)
+
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+
+
+if __name__ == '__main__':
+	if len(sys.argv) > 1:
+		if sys.argv[1] == "check_syntax":
+			print("Build [ OK ]")
+			exit(0)
+		else:
+			print("Passed argument not supported ! Supported argument : check_syntax")
+			exit(1)
+	app.run(debug=True)
---
+++ td/td.py
@@ -0,0 +1,79 @@
+from flask import Flask, request
+from flask_cors import CORS
+from flask_swagger_ui import get_swaggerui_blueprint
+from datetime import datetime
+import sqlite3
+from markupsafe import escape
+import sys
+import pandas as pd
+from flask import jsonify
+import redis
+
+app = Flask(__name__)
+
+r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+cals = []
+
+@app.route('/', methods=['GET'])
+def hello():
+	if request.method == 'GET':
+		return "HELLO! url api docs http://127.0.0.1:5000/api/cal/ \n\
+			http://127.0.0.1:5000/api/cal/addition/a/b/key \n\
+			http://127.0.0.1:5000/api/cal/soustraction/a/b/key \n\
+			http://127.0.0.1:5000/api/cal/division/a/b/key \n\
+			http://127.0.0.1:5000/api/cal/multiplication/a/b/key "
+	
+@app.route('/api/cal/', methods=['GET'])
+def get_result():
+	if request.method == 'GET':
+		return cals
+
+@app.route('/api/cal/addition/<int:a>/<int:b>/<string:key>', methods=['GET'])
+def addition(a,b,key):
+	add = a+b
+	cal = {key,add}
+	map = r.hset("addition",mapping={
+		key: add
+    })
+	return jsonify(r.hgetall("addition"))
+
+@app.route('/api/cal/soustraction/<int:a>/<int:b>/<string:key>', methods=['GET'])
+def soustraction(key,a,b):
+	sous = a-b
+	cal = {key,sous}
+	r.hset("soustraction",mapping={
+		key: sous
+    })
+	return jsonify(r.hgetall("soustraction"))
+
+@app.route('/api/cal/division/<int:a>/<int:b>/<string:key>', methods=['GET'])
+def division(key,a,b):
+	if (b == 0):
+		error = {"error": "impossible"}
+		return jsonify(error, 400)
+	div = a/b
+	cal = {key,div}
+	map = r.hset("division",mapping={
+		key: div
+    })
+	return jsonify(r.hgetall("division"))
+
+@app.route('/api/cal/multiplication/<int:a>/<int:b>/<string:key>', methods=['GET'])
+def multiplication(key,a,b):
+	mul = a*b
+	cal = {key,mul}
+	map = r.hset("multiplication",mapping={
+		key: mul
+    })
+	return jsonify(r.hgetall("multiplication"))
+	
+
+if __name__ == '__main__':
+	if len(sys.argv) > 1:
+		if sys.argv[1] == "check_syntax":
+			print("Build [ OK ]")
+			exit(0)
+		else:
+			print("Passed argument not supported ! Supported argument : check_syntax")
+			exit(1)
+	app.run(debug=True)
